99262bd9b4700ebacda4721cb07dd7af3fd03724
서빈 <r25664@naver.com>
2024-04-28 21:07:43 +0900

    > Weather api를 구현하기 위해 xls 파일 읽는 reader 추가 > 오류 처리 작업 중...

--- app/src/main/java/com/example/project/WeatherData.java
+++ app/src/main/java/com/example/project/WeatherData.java
@@ -1,29 +1,48 @@
 package com.example.project;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.WorkSource;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 public class WeatherData {
 
-    public String lookUpWeather(String date, String time, String nx, String ny) throws IOException {
-        String baseDate = date; // 2022xxxx 형식을 사용해야 함
-        String baseTime = time; // 0500 형식을 사용해야 함
+    private String weather = "", tmperature = "";
+
+    public String lookUpWeather(String baseDate, String time, String nx, String ny) throws IOException, JSONException {
+
+        // String baseDate = date;
+        // "20210531";
+        // date 조회하고싶은 날짜
+        String baseTime = timeChange(time);
+        //"0500";//timeChange(time);	//조회하고싶은 시간
         String type = "json";
 
-        // end point 주소값
         String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
-        // 일반 인증키
+        //         홈페이지에서 받은 키
         String serviceKey = "lMVeP6C%2FH0HG0vEJ4pCnrRb3MVCmPBPs0AwzeSYZdMV%2Fxtkveko2nQ%2F8iSUcRbF7Ce5k62hw%2Bd6LF9clum1k9Q%3D%3D";
 
         StringBuilder urlBuilder = new StringBuilder(apiUrl);
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey); // 서비스 키
-        urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); // x좌표
-        urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); // y좌표
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("14", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
+        urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); //경도
+        urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); //위도
         urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /* 조회하고싶은 날짜*/
         urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /* 조회하고싶은 시간 AM 02시부터 3시간 단위 */
         urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode(type, "UTF-8"));    /* 타입 */
@@ -32,8 +51,8 @@ public class WeatherData {
          * GET방식으로 전송해서 파라미터 받아오기
          */
         URL url = new URL(urlBuilder.toString());
-        // json데이터들을 웹페이지를통해 확인할 수 있게  로그캣에 링크 출력
-        System.out.println("url : " + url);
+        //어떻게 넘어가는지 확인하고 싶으면 아래 출력분 주석 해제
+        //System.out.println(url);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
@@ -51,6 +70,107 @@ public class WeatherData {
         }
         rd.close();
         conn.disconnect();
-        return sb.toString();
+        String result = sb.toString();
+
+
+        // response 키를 가지고 데이터를 파싱
+        JSONObject jsonObj_1 = new JSONObject(result);
+        String response = jsonObj_1.getString("response");
+
+        // response 로 부터 body 찾기
+        JSONObject jsonObj_2 = new JSONObject(response);
+        String body = jsonObj_2.getString("body");
+
+        // body 로 부터 items 찾기
+        JSONObject jsonObj_3 = new JSONObject(body);
+        String items = jsonObj_3.getString("items");
+        Log.i("ITEMS", items);
+
+        // items로 부터 itemlist 를 받기
+        JSONObject jsonObj_4 = new JSONObject(items);
+        JSONArray jsonArray = jsonObj_4.getJSONArray("item");
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            jsonObj_4 = jsonArray.getJSONObject(i);
+            String fcstValue = jsonObj_4.getString("fcstValue");
+            String category = jsonObj_4.getString("category");
+
+            if (category.equals("SKY")) {
+                weather = "현재 날씨는 ";
+                if (fcstValue.equals("1")) {
+                    weather += "맑은 상태로";
+                } else if (fcstValue.equals("2")) {
+                    weather += "비가 오는 상태로 ";
+                } else if (fcstValue.equals("3")) {
+                    weather += "구름이 많은 상태로 ";
+                } else if (fcstValue.equals("4")) {
+                    weather += "흐린 상태로 ";
+                }
+            }
+
+            if (category.equals("T3H") || category.equals("T1H")) {
+                tmperature = "기온은 " + fcstValue + "℃";
+            }
+
+            Log.i("날씨", fcstValue);
+            Log.i("카테고리", category);
+
+            Log.i("지금 날씨는", weather + tmperature);
+        }
+
+        return weather + tmperature;
     }
+
+    public String timeChange(String time)
+    {
+        // 현재 시간에 따라 데이터 시간 설정(3시간 마다 업데이트) //
+        /**
+         시간은 3시간 단위로 조회해야 한다. 안그러면 정보가 없다고 뜬다.
+         0200, 0500, 0800 ~ 2300까지
+         그래서 시간을 입력했을때 switch문으로 조회 가능한 시간대로 변경해주었다.
+         **/
+        switch(time) {
+
+            case "0200":
+            case "0300":
+            case "0400":
+                time = "0200";
+                break;
+            case "0500":
+            case "0600":
+            case "0700":
+                time = "0500";
+                break;
+            case "0800":
+            case "0900":
+            case "1000":
+                time = "0800";
+                break;
+            case "1100":
+            case "1200":
+            case "1300":
+                time = "1100";
+                break;
+            case "1400":
+            case "1500":
+            case "1600":
+                time = "1400";
+                break;
+            case "1700":
+            case "1800":
+            case "1900":
+                time = "1700";
+                break;
+            case "2000":
+            case "2100":
+            case "2200":
+                time = "2000";
+                break;
+            default:
+                time = "2300";
+
+        }
+        return time;
+    }
+
 }
--- app/src/main/java/com/example/project/Weather_Activity.java
+++ app/src/main/java/com/example/project/Weather_Activity.java
@@ -1,18 +1,88 @@
 package com.example.project;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 public class Weather_Activity extends AppCompatActivity {
+
+    private String x = "", y = "", address = "";
+
+    private String date = "", time = "";
+
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+        setContentView(R.layout.activity_main);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+    }
+
+    public void readExcel(String localName) {
+        try {
+            InputStream is = getBaseContext().getResources().getAssets().open("local.xls");
+            Workbook wb = Workbook.getWorkbook(is);
+
+            if (wb != null) {
+                Sheet sheet = wb.getSheet(0);   // 시트 불러오기
+                if (sheet != null) {
+                    int colTotal = sheet.getColumns();    // 전체 컬럼
+                    int rowIndexStart = 1;                  // row 인덱스 시작
+                    int rowTotal = sheet.getColumn(colTotal - 1).length;
+
+                    for (int row = rowIndexStart; row < rowTotal; row++) {
+                        String contents = sheet.getCell(0, row).getContents();
+                        if (contents.contains(localName)) {
+                            x = sheet.getCell(1, row).getContents();
+                            y = sheet.getCell(2, row).getContents();
+                            row = rowTotal;
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            Log.i("READ_EXCEL1", e.getMessage());
+            e.printStackTrace();
+        } catch (BiffException e) {
+            Log.i("READ_EXCEL1", e.getMessage());
+            e.printStackTrace();
+        }
+        Log.i("격자값", "x = " + x + "  y = " + y);
+    }
+
+    public String getCurrentAddress(double latitude, double longitude) {
+        String address = "";
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address currentAddress = addresses.get(0);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i <= currentAddress.getMaxAddressLineIndex(); i++) {
+                    sb.append(currentAddress.getAddressLine(i)).append(" ");
+                }
+                address = sb.toString();
+            }
+        } catch (IOException e) {
+            Log.e("Geocoder", "Error getting address from location", e);
+        }
+
+        return address;
     }
 }
